4eb203ed06b0498280ee45e82d1211ec48609236
Greg R <roundsgreg@gmail.com>
2023-11-11 12:52:07 -0600

    Adding Produc search to target_search feature file

--- features/target_projects/steps/target_search.py
+++ features/target_projects/steps/target_search.py
@@ -11,15 +11,40 @@ def open_target(context):
 @when('Search for {product}')
 def search_product(context, product):
     context.driver.find_element(By.ID, 'search').send_keys(product)
+    context.product = product
     context.driver.find_element(By.XPATH, "//button[@data-test='@web/Search/SearchButton']").click()
     sleep(6)
 
 
 @then('Verify search worked for {product}')
 def verify_search(context, product):
-    search_results = context.driver.find_element(By.XPATH, "//h2[contains(@class,'styles__StyledHeading')]/span[@class='h-margin-r-x2']").text
-    assert product in search_results , f"{product}, is not in {search_results}"
+    search_results = context.driver.find_element(By.XPATH,
+                                                 "//h2[contains(@class,'styles__StyledHeading')]/span[@class='h-margin-r-x2']").text
+    assert product in search_results, f"{product}, is not in {search_results}"
+
+
 @then('Verify {product} in search result url')
 def verify_search_url(context, product):
     search_url = context.driver.current_url
     assert product in search_url, f"{product}, is not in {search_url}"
+
+
+@then('Verify item is found and select cart')
+def select_cart(context):
+    items = context.driver.find_elements(By.XPATH, "//div[@class='h-display-flex h-flex-justify-space-between']")
+    add_to_cart = context.driver.find_elements(By.XPATH, f"//button[contains(@id, 'addToCartButton')]")
+    counter = 0
+    for item in items:
+        if context.product in item.text:
+            context.fullProductsName = item.text
+            add_to_cart[counter].click()
+            break
+        else:
+            counter += 1
+    context.driver.find_element(By.XPATH, "//button[@aria-label='close']").click()
+
+
+@then('Verify item is in cart')
+def verify_item_is_in_cart(context):
+    item_in_cart = context.driver.find_element(By.XPATH, "//div[@data-test='cartItem-title']").text
+    assert item_in_cart == context.fullProductsName , f"item in cart ('{item_in_cart}') does not match products name ('{context.fullProductsName}')"
--- features/target_projects/tests/target_search.feature
+++ features/target_projects/tests/target_search.feature
@@ -5,11 +5,19 @@ Feature: Search tests
         Then Verify search worked for coffee
         And Verify coffee in search result url
 
-    Scenario: User can search for tea
+    Scenario: User can search for products
         Given Open target main page
         When search for tea
         Then Verify search worked for tea
         And Verify tea in search result url
+
+    Scenario: User can add to cart
+        Given Open target main page
+        When search for AirPods (3rd Generation)
+        Then Verify item is found and select cart
+        When User selects the cart button
+        Then Verify item is in cart
+
 
     Scenario Outline: User can search for a product
     Given Open target main page
